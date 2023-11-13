5d61293051806f0aeef427a3e45e5a321361beac
zhihan <zhihan.scut@gmail.com>
2023-11-13 21:53:17 +0800

    Update server.py

--- server/server.py
+++ server/server.py
@@ -2,7 +2,7 @@ import socket
 import json
 import time
 
-host = '0.0.0.0'
+host = '0.0.0.0' # 允许任何IP地址连接
 port = 12345
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,8 +12,15 @@ server_socket.listen(5)  # 允许多个客户端连接
 print(f"监听 Port: {port}")
 
 while True:
-    client_socket, client_address = server_socket.accept()
-    print(f"连接来自 {client_address}")
+
+    server_socket.settimeout(1) # 设置超时时间
+    while True:
+        try:
+            client_socket, client_address = server_socket.accept()
+            break
+        except socket.timeout:
+            continue
+    
 
     while True:
         data = client_socket.recv(1024)
@@ -25,11 +32,20 @@ while True:
         except json.JSONDecodeError:
             print("非JSON数据")
             break
+        
+        print(f"连接来自 {client_address} 设备名称： " + decoded_data["Device"])
 
         # 在这里进行回复
+
         if(decoded_data["Device"] == "Dual Arm Robot"): # 双臂机器人
             RobotState = decoded_data["State"]
             print(f"双臂机器人状态: {RobotState}")
+            response = json.dumps({
+                    "TimeStamps": time.time(), # 时间戳
+                    "Command": 7, # 命令的编号
+                })
+        if(decoded_data["Device"] == "Raspberry Pi"): # 树莓派
+            RspState = decoded_data["State"]
             response = json.dumps({
                     "TimeStamps": time.time(), # 时间戳
                     "Command": 7, # 命令的编号
